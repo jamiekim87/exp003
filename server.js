96de91d170f80efed2f41bdff05d16a3bacd7491
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-08-18 14:33:01 -0700

    commit

--- server.js
+++ server.js
@@ -7,27 +7,6 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-let items = []
-
-// GET all items
-app.get('/items', (req, res) => {
-    res.json(items)
-})
-
-// POST one item
-app.post('/items', (req, res) => {
-    items.push(req.body)
-    res.sendStatus(200)
-})
-
-// PUT one item
-app.put('/items/:text', (req, res) => {
-
-})
-
-// DELETE one item
-app.delete('/items/:text', (req, res) => {
-
-})
+app.use(require('./routes'))
 
 app.listen(3000)
