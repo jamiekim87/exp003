ceb2359936aff0d86741ec8b58bb8971960768b7
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-08-18 10:57:44 -0700

    commit

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const express = require('express')
 const app = express()
 
 app.get('/test', (req, res) => {
-    res.send('hi')
+    console.log(req.query.n)
+    res.send(`Hello ${req.query.n}!`)
+})
+
+app.get('/name/:n', (req, res) => {
+    console.log(req.params.n)
+    res.send(`Hello ${req.params.n}!`)
 })
 
 app.listen(3000)
