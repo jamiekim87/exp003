33b1b7129fcee38ac66e109da44d11122c6dfbe7
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-08-18 12:00:33 -0700

    push

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const { join } = require('path')
 
 const app = express()
 
+app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.listen(3000)
+// GET all items
+app.get('/items', (req, res) => {
 
+})
+
+// POST one item
+app.post('/items', (req, res) => {
+
+})
+
+// PUT one item
+app.put('/items/:text', (req, res) => {
+
+})
+
+// DELETE one item
+app.delete('/items/:text', (req, res) => {
+
+})
+
+app.listen(3000)
